2b7dbc6a19d5f4ecc98b3cfa0c4577c2166cdec0
zdurr22 <zdurr22@gmail.com>
2021-04-05 16:55:57 -0400

    commit tree 346e4e52c1c6378ed2c998f4217d0badc3326005

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@ships = []
+
+    def initialize(attributes)
+        @name = attributes[:name]
+        @type = attributes[:type]
+        @booty = attributes[:booty]
+
+        @@ships << self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
 end
